b827221d62e4e13600c4dc04688dac1339b65502
vmonney <45258650+vmonney@users.noreply.github.com>
2026-01-16 16:44:00 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1])
+
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f'hello pipeline, month={month}')
