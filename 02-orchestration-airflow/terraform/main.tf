224403072520907bffcbd2f81a2cd2471376267d
vmonney <45258650+vmonney@users.noreply.github.com>
2026-02-18 14:54:22 +0100

    Add analytics engineering project structure with dbt integration.

--- 02-orchestration-airflow/terraform/main.tf
+++ 02-orchestration-airflow/terraform/main.tf
@@ -29,6 +29,7 @@ resource "google_storage_bucket" "zoomcamp-bucket" {
 }
 
 resource "google_bigquery_dataset" "zoomcamp-dataset" {
-  dataset_id = var.bq_dataset_name
-  location   = var.location
+  dataset_id                 = var.bq_dataset_name
+  location                   = var.location
+  delete_contents_on_destroy = true
 }
